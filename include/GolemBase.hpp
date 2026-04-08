ad0aa3333ba7a03242d89aa21fa51b6f26d40689
kbar-sys <kevinbarnard82@googlemail.com>
2026-04-08 16:54:05 +0100

    first

---
+++ include/GolemBase.hpp
@@ -0,0 +1,51 @@
+#ifndef GOLEMBASE_HPP_INCLUDED
+#define GOLEMBASE_HPP_INCLUDED
+
+#include "irrlicht.h"
+#include "lo3items.hpp"
+
+struct GolemBase
+{
+    irr::scene::ICameraSceneNode * camera;
+    irr::scene::IBillboardSceneNode * billboard;
+
+	irr::video::ITexture * golem_sprites[LO3_SPR_GOLEM];
+
+
+    void initMonst();
+    void setBillboard(irr::scene::ISceneManager * sm, irr::core::vector3df position);
+    void setTextures(irr::video::ITexture ** copy_of_tex, int size);
+    void beforeRun(irr::IrrlichtDevice * device);
+    void beforeDraw(irr::IrrlichtDevice * device);
+
+    void outsideMonst(irr::scene::ICameraSceneNode * camera);
+    void directionMonst();
+    void renderMonst();
+
+
+    irr::core::vector3df forward;
+
+	irr::core::vector3df v3dfCameraPosition;
+	irr::core::vector3df v3dfBillboardposition;
+	irr::core::vector3df relativeDirection;
+	irr::core::vector3df billboardDirection;
+
+
+	irr::f32 angleY_rad;
+    irr::f32 angleY_degrees;
+	irr::u32 currentFrame;
+	irr::u32 msFrameRate;
+
+	irr::u32 lastTime;
+    irr::u32 currentTime;
+    irr::u32 elapsedTime;
+
+    GolemBase()
+    {
+        forward = irr::core::vector3df(0.f, 0.f, -1.f);
+        msFrameRate = 124;
+        currentFrame = 0;
+    }
+};
+
+#endif // GOLEMBASE_HPP_INCLUDED
---
+++ include/lo3items.hpp
@@ -0,0 +1,29 @@
+#ifndef ITEMS_HPP_INCLUDED
+#define ITEMS_HPP_INCLUDED
+
+//#include "irrlicht.h"
+
+// ENUMS
+
+enum EITEMS
+{
+    GUI_ID_QUIT = 101
+};
+
+enum LO3_ITEMS
+{
+    LO3_UNINIT = -1, // for IrrEdit quickness
+    LO3_NOTHING = 0,
+    LO3_PLAYER = 1,
+    LO3_GOLEM = 2,
+    LO3_BEHEM = 3,
+    LO3_HEALTH = 10
+};
+
+enum LO3_SPRITES
+{
+    LO3_SPR_GOLEM = 112
+};
+
+
+#endif // ITEMS_HPP_INCLUDED
